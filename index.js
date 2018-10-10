83b3a8ef5c46d1ad5d114aa817cb33a958ae0e8f
Shubham Mehrotra <shubhammehrotra.symfony@webkul.com>
2018-10-10 10:57:35 +0530

    update

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 "use strict"
 
-const fs = require('fs');           // interact with other files
-const url = require('url');
-const path = require('path');
-const http = require('http');
-const socket = require('socket.io');
 const IO = require('./io.js');
 const shared = require('./sharable.js');
 const subscriber = require('./subscriber.js');
@@ -12,38 +7,20 @@ const subscriber = require('./subscriber.js');
 const express = require('express');
 const app = express();
 
-var getRequiredFieldsFromUrl = (request) => {
-    // Get the url and parse it
-    let parsedUrl = url.parse(request.url, true);
-    
-    // Get the path
-    let path = parsedUrl.pathname;
-    let trimmedPath = path.replace(/^\/+|\/+$/g, '')
-    
-    let queryStringObject = parsedUrl.query;
-    
-    let requiredFields = {
-        trimmedPath: trimmedPath,
-        queryParams: queryStringObject,
-    }
-    
-    return requiredFields;
-}
+const server = require('http').Server(app);
+new IO(server);
 
 app.set('view-engine', 'jade');
 
 app.get(['/summer', '/winter', '/database', '/sendmail', '/chatwizz'], (req, res) => {
-    let fields = getRequiredFieldsFromUrl(req);
+    let fields = sharedgetRequiredFieldsFromUrl(req);
     let trimmedPath = fields.trimmedPath;
     let queryParams = fields.queryParams;
 
     subscriber.emit(trimmedPath, queryParams);
-    if(trimmedPath == 'chatwizz') res.sendFile(path.join(__dirname+'/index.html'));
+    if(trimmedPath == 'chatwizz') res.sendFile(path.join(__dirname+'/public/index.html'));
     else res.send(subscriber.eventResponse);
 })
-
-var server = http.Server(app);
-new IO(server);
 
 // Start the HTTP server
 server.listen(shared.port, () => {
--- sharable.js
+++ sharable.js
@@ -1,10 +1,31 @@
 "use strict"
 
+const url = require('url');
+
 var port = 7000;
 // Error Handler for promises
 var withErrorHandler = (fn) => fn.catch((error) => console.log("Error: ", error));
 
+var getRequiredFieldsFromUrl = (request) => {
+    // Get the url and parse it
+    let parsedUrl = url.parse(request.url, true);
+    
+    // Get the path
+    let path = parsedUrl.pathname;
+    let trimmedPath = path.replace(/^\/+|\/+$/g, '')
+    
+    let queryStringObject = parsedUrl.query;
+    
+    let requiredFields = {
+        trimmedPath: trimmedPath,
+        queryParams: queryStringObject,
+    }
+    
+    return requiredFields;
+}
+
 module.exports = {
-    withErrorHandler: withErrorHandler,
     port: port,
+    withErrorHandler: withErrorHandler,
+    getRequiredFieldsFromUrl: getRequiredFieldsFromUrl,
 }
